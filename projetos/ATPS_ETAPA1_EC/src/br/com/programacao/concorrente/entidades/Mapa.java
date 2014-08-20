f593defc33958a05cea3bed0d581905584fb275b
afonso.rodrigues <afonsoaugustoventura@gmail.com>
2014-08-20 18:06:46 -0300

    inclusão do novo projeto no eclipse

---
+++ projetos/ATPS_ETAPA1_EC/src/br/com/programacao/concorrente/entidades/Mapa.java
@@ -0,0 +1,27 @@
+package br.com.programacao.concorrente.entidades;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Mapa {
+	private static Map<String, File> mapa = createMapaArquivos();
+	private static List<String> listaIndicies = createListaIndicies();
+
+	private static Map<String, File> createMapaArquivos() {
+		Map<String, File> mapa = new HashMap<String, File>();
+		
+		
+		
+		return mapa;
+	}
+
+	private static List<String> createListaIndicies() {
+		List<String> lista = new ArrayList<String>();
+		return null;
+	}
+	
+
+}
